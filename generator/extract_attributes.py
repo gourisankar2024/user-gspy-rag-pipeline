04bac3d28b4d7eea274bb9166e95302cde70ecc7
Yaswanth Raj Kumar Yadiki <yaswanthraj671@gmail.com>
2024-12-15 14:03:38 +0530

    Modified logic to split relevant docs and response

--- generator/extract_attributes.py
+++ generator/extract_attributes.py
@@ -1,19 +1,37 @@
-import json
+
 from generator.create_prompt import create_prompt
 from generator.initialize_llm import initialize_llm
 
 # Initialize the LLM
 llm = initialize_llm()
 
+
 # Function to extract attributes
 def extract_attributes(question, relevant_docs, response):
     # Format documents into a string by accessing the `page_content` attribute of each Document
-    formatted_documents = "\n".join([f"Doc {i+1}: {doc.page_content}" for i, doc in enumerate(relevant_docs)])
-
-    #print(f'Formated documents: {formatted_documents}')
-    attribute_prompt = create_prompt(formatted_documents, question, response)
+    formatted_documents = split_relevant_documents(relevant_docs)
+    formatted_response = split_response(response)
+    # print(f'Formatted documents: {formatted_documents}')
+    attribute_prompt = create_prompt(formatted_documents, question, formatted_response)
 
     # Instead of using BaseMessage, pass the formatted prompt directly to invoke
     result = llm.invoke(attribute_prompt)
 
     return result
+
+
+def split_relevant_documents(relevant_docs: list) -> list:
+    split_relevant_documents_list = []
+    for relevant_doc_index, relevant_doc in enumerate(relevant_docs):
+        sentences = []
+        for sentence_index, sentence in enumerate(relevant_doc.page_content.split(".")):
+            sentences.append([str(relevant_doc_index)+chr(97 + sentence_index), sentence])
+        split_relevant_documents_list.append(sentences)
+    return split_relevant_documents_list
+
+
+def split_response(response: str):
+    sentences = []
+    for sentence_index, sentence in enumerate(response.split(".")):
+        sentences.append([chr(97 + sentence_index), sentence])
+    return sentences
