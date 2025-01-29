642c7ca6d749738c8ae0dafa40356d5834dfefac
Gourisankar Padihary <gourisankar.padihary@simcorp.com>
2025-01-29 14:22:22 +0530

    removed api key

--- generator/initialize_llm.py
+++ generator/initialize_llm.py
@@ -3,14 +3,14 @@ import os
 from langchain_groq import ChatGroq
 
 def initialize_generation_llm():
-    os.environ["GROQ_API_KEY"] = "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"
+    os.environ["GROQ_API_KEY"] = ""
     model_name = "llama3-8b-8192"
     llm = ChatGroq(model=model_name, temperature=0.7)
     logging.info(f'Generation LLM {model_name} initialized')
     return llm
 
 def initialize_validation_llm():
-    os.environ["GROQ_API_KEY"] = "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"
+    os.environ["GROQ_API_KEY"] = ""
     model_name = "llama3-70b-8192"
     llm = ChatGroq(model=model_name, temperature=0.7)
     logging.info(f'Validation LLM {model_name} initialized')
