5661370a5068a6d643d8d093ed09e37e85ac5af1
Gourisankar Padihary <gourisankar.padihary@simcorp.com>
2025-01-29 14:24:00 +0530

    Model name parameterized

--- generator/initialize_llm.py
+++ generator/initialize_llm.py
@@ -8,7 +8,6 @@ def initialize_generation_llm():
     os.environ["GROQ_API_KEY"] = ""
     model_name = ConfigConstants.GENERATION_MODEL_NAME
     llm = ChatGroq(model=model_name, temperature=0.7)
-    llm.name = model_name
     logging.info(f'Generation LLM {model_name} initialized')
     return llm
 
@@ -16,6 +15,5 @@ def initialize_validation_llm():
     os.environ["GROQ_API_KEY"] = ""
     model_name = ConfigConstants.VALIDATION_MODEL_NAME
     llm = ChatGroq(model=model_name, temperature=0.7)
-    llm.name = model_name
     logging.info(f'Validation LLM {model_name} initialized')
     return llm
